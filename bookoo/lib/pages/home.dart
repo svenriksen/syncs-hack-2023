e41451530369c0f3920c909227cd893019073ff1
Tran Nguyen Sinh <kaisvenriksen@gmail.com>
2023-08-26 19:13:53 +1000

    done home

--- bookoo/lib/pages/home.dart
+++ bookoo/lib/pages/home.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:google_sign_in/widgets.dart';
 // import 'package:flutter/widgets.dart';
+
+import 'firstpage.dart';
 
 class Home extends StatefulWidget {
   Home({super.key, required this.url});
@@ -13,6 +13,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late GoogleIdentity avatar = widget.url;
+  int pageIndex = 0;
   @override
   void initState() {
     avatar = widget.url;
@@ -23,57 +24,43 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          title: const Text("Home"),
-          elevation: 5,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 15),
-              child: GoogleUserCircleAvatar(identity: avatar),
-            )
-          ],
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (value) {
+          setState(() {
+            debugPrint(value.toString());
+            pageIndex = value;
+          });
+        },
+        selectedIndex: pageIndex,
+        destinations: const [
+          NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: "Home"),
+          NavigationDestination(
+              selectedIcon: Icon(Icons.search_off_outlined),
+              icon: Icon(Icons.search_outlined),
+              label: "Find Book"),
+        ],
+      ),
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text("Home"),
+        elevation: 5,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15),
+            child: GoogleUserCircleAvatar(identity: avatar),
+          )
+        ],
+      ),
+      body: <Widget>[
+        const SingleChildScrollView(
+          child: Padding(
+              padding: EdgeInsets.fromLTRB(15, 50, 15, 0), child: FirstPage()),
         ),
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(15, 50, 15, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Your book choices",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              const Text(
-                "Return to your past read books :)",
-                style: TextStyle(fontSize: 20),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: CarouselSlider(
-                    items: [1, 2, 3]
-                        .map((e) => Builder(builder: (BuildContext context) {
-                              return Container(
-                                margin:
-                                    const EdgeInsets.symmetric(horizontal: 5.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(20)),
-                                  color: Theme.of(context).colorScheme.primary,
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    'text $e',
-                                    style: const TextStyle(
-                                        fontSize: 16.0, color: Colors.white),
-                                  ),
-                                ),
-                              );
-                            }))
-                        .toList(),
-                    options: CarouselOptions(height: 400, autoPlay: false)),
-              ),
-            ],
-          ),
-        ));
+        const Text("Find Book"),
+      ][pageIndex],
+    );
   }
 }
