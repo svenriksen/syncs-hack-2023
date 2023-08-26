84ad8fad205055c5b132594d45a17147d2e7b01e
Lam Le <40801045+lalalkila@users.noreply.github.com>
2023-08-26 14:27:18 +1000

    add api server

---
+++ server/server.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Sample data - replace this with your own data source or database
+user_profiles = {}
+
+@app.route('/recommend', methods=['POST'])
+def recommend_books():
+    data = request.json
+
+    acc_id = data.get('accID')
+    prompt = data.get('prompt')
+    past_books = data.get('pastBooks', [])
+
+    print(f'Incoming request from {acc_id}. Given prompt: "{prompt}"')
+    for b in past_books:
+        print(f'''---
+name: {b.get("name")}
+read {b.get("nread")} times
+ratings given: {b.get("ratings")}
+''')
+
+    if not acc_id or not prompt:
+        return jsonify({'error': 'Account ID and prompt are required'}), 400
+
+    user_profiles[acc_id] = {
+        'prompt': prompt,
+        'past_books': past_books
+    }
+
+    recommended_books = recommend_books_for_user(past_books)  # Replace with your recommendation logic
+
+    return jsonify({'recommendedBooks': recommended_books})
+
+def recommend_books_for_user(past_books):
+    # Replace this with your recommendation logic
+    # You can use the past_books information to make recommendations based on reading history and ratings
+    # Return a list of recommended books with their details
+    recommended_books = past_books
+    return recommended_books
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
