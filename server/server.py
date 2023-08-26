9e0cf268e77fb4f1832e7e4d8590a41fcf4f9d93
Lam Le <40801045+lalalkila@users.noreply.github.com>
2023-08-27 08:20:35 +1000

    fix dev

--- server/server.py
+++ server/server.py
@@ -31,6 +31,6 @@ def recommend_books():
 
     return recommended_books
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
 
