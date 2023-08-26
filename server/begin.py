b2ae19c932572f34493d1c07aca2b44c73878295
unknown <bonbon.muoiot@gmail.com>
2023-08-26 14:38:05 +1000

    prompt done probably

--- server/begin.py
+++ server/begin.py
@@ -1,19 +1,25 @@
+from objects import User,Book
 from getpass import getpass
 CLARIFAI_PAT = getpass()
 from langchain.llms import Clarifai
 from langchain import PromptTemplate, LLMChain
+import re
 template = """Question: {question}
 
-Answer: Give me ten. Inlcude a link to these book on amazon and return them in json format"""
+Answer: Short and to the point. Give me 10 and link on amazon."""
 
 prompt = PromptTemplate(template=template, input_variables=["question"])
-USER_ID = "openai"
+USER_ID = "openai" #info about model
 APP_ID = "chat-completion"
 MODEL_ID = "GPT-3_5-turbo"
+
 clarifai_llm = Clarifai(
     pat=CLARIFAI_PAT, user_id=USER_ID, app_id=APP_ID, model_id=MODEL_ID
 )
+
 llm_chain = LLMChain(prompt=prompt, llm=clarifai_llm)
-question = "What book title would a person who likes cooking like to read?"
-print(llm_chain.run(question))
-        
+question = "What book title would a person who likes cooking like to read? return the name of the books and the url of each book on amazon in json."
+# run chain and store result
+result = llm_chain.run(question)
+links = list(result.split("\n"))
+print(result)
--- server/objects.py
+++ server/objects.py
@@ -6,8 +6,9 @@ class User:
         return self.accID
 
 class Book:
-    def __init__(self, name : str, rating : int = None, n_read : int = 1):
+    def __init__(self, name : str, rating : int = None, n_read : int = 1, url : str = None):
         self.name = name
         self.rating = rating
         self.n_read = n_read
+        self.url = url
 
