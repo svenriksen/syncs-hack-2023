ecb96c38f6ecbcf99ae1b20d0b84b3ebb6e226aa
unknown <bonbon.muoiot@gmail.com>
2023-08-26 20:50:01 +1000

    Change to HuggingFace

--- server/prompt.py
+++ server/prompt.py
@@ -1,9 +1,10 @@
 from objects import User,Book
 import settings
-CLARIFAI_PAT = settings.OPENAI_API_KEY
+import os
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = settings.API_KEY
+from langchain import HuggingFaceHub
 from langchain.llms import Clarifai
 from langchain import PromptTemplate, LLMChain
-import re
 import json
 template = """Question: {question}
 Additional information, these are books that users have read, it might have ratings on a scale of 10 (1 is the lowest and 10 is the highest). Please based on the book that they read, how many times they read them, and their ratings for the book to better evalute your reccommendation of books. Below are the books
@@ -12,15 +13,10 @@ Additional information, these are books that users have read, it might have rati
 Answer: Short and to the point. Give me 10 and link on amazon. Please return the name of the books and the url of each book on amazon in json and store it in the variable called books."""
 
 prompt = PromptTemplate(template=template, input_variables=["question", "books"])
-USER_ID = "openai" #info about model
-APP_ID = "chat-completion"
-MODEL_ID = "GPT-3_5-turbo"
 
-clarifai_llm = Clarifai(
-    pat=CLARIFAI_PAT, user_id=USER_ID, app_id=APP_ID, model_id=MODEL_ID
-)
+huggingface_llm = HuggingFaceHub(repo_id="google/flan-t5-xl", model_kwargs={"temperature":0, "max_length":1000})
 
-llm_chain = LLMChain(prompt=prompt, llm=clarifai_llm)
+llm_chain = LLMChain(prompt=prompt, llm=huggingface_llm)
 
 def getRec(prompt : str, books):
     a = llm_chain.run({'question':prompt, 'books':books})
@@ -34,4 +30,4 @@ def getRec(prompt : str, books):
 if __name__ == "__main__":
     question = "What book title would a person who likes cooking like to read? return the name of the books and the url of each book on amazon in json."
     # run chain and store result
-    print(getRec(question))
+    print(getRec(question,[]))
