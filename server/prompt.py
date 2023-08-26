9a1dcbea48f6248e37297fea559d0d260471ccad
unknown <bonbon.muoiot@gmail.com>
2023-08-27 02:17:14 +1000

    BRUH

--- server/prompt.py
+++ server/prompt.py
@@ -1,24 +1,22 @@
 from objects import User,Book
-import settings,os
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_KEY",default="")
+from getpass import getpass
+CLARIFAI_PAT = getpass()
+from langchain.llms import Clarifai
 from langchain import PromptTemplate, LLMChain
-import json
-template = """Question: {question}.give me 10 and link on amazon. Return the name of the books and the url of each book on amazon"""# in json list.
-#"""
-#Answer: Short and to the point. """
-from langchain.llms import OpenAI
+template = """Question: {question}
+Answer: Short and to the point. Give me 10 and link on amazon."""
 
 prompt = PromptTemplate(template=template, input_variables=["question"])
+USER_ID = "openai" #info about model
+APP_ID = "chat-completion"
+MODEL_ID = "GPT-3_5-turbo"
 
-openai_llm = OpenAI()
+clarifai_llm = Clarifai(
+    pat=CLARIFAI_PAT, user_id=USER_ID, app_id=APP_ID, model_id=MODEL_ID
+)
 
-llm_chain = LLMChain(prompt=prompt, llm=openai_llm)
-
-def getRec(prompt : str):
-    a = llm_chain.run(prompt)
-    return a
-
-if __name__ == "__main__":
-    question = "What book title would a person who likes cooking like to read?"
-    # run chain and store result
-    print(getRec(question))
+llm_chain = LLMChain(prompt=prompt, llm=clarifai_llm)
+question = "What book title would a person who likes cooking like to read? return the name of the books and the url of each book on amazon in json."
+# run chain and store result
+result = llm_chain.run(question)
+print(result)
