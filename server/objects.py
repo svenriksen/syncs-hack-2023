530f671d95cf55fb8953a66fd38e48afead34f82
Lam Le <40801045+lalalkila@users.noreply.github.com>
2023-08-26 12:30:49 +1000

    add objs

---
+++ server/objects.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self, accID : str):
+        self.accID : str = accID
+
+    def getID(self):
+        return self.accID
+
+class Book:
+    def __init__(self, name : str, rating : int = None, n_read : int = 1):
+        self.name = name
+        self.rating = rating
+        self.n_read = n_read
+
